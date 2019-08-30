256de1b5a11460ecad6ed43919a7c3edbfcf7e40
Abhijith V Mohan <abhijith.vmohan@gmail.com>
2019-08-31 04:22:21 +0530

    Override configs

---
+++ private/config.lua
@@ -0,0 +1,23 @@
+hspoon_list = {
+  "AClock",
+  "CountDown",
+  "HCalendar",
+  "HSearch",
+  "SpeedMenu",
+  "WinWin",
+  "FnMate"
+}
+
+hsapp_list = {
+  {key = "a", name = "Google Chrome"},
+  {key = "w", name = "Slack"},
+  {key = "j", name = "IntelliJ IDEA CE"},
+  {key = "k", name = "Visual Studio Code"},
+  {key = "m", id = "com.apple.ActivityMonitor"},
+  {key = ",", id = "com.apple.systempreferences"}
+}
+
+-- Disable alt-L lock
+hslock_keys = {"", ""}
+
+
